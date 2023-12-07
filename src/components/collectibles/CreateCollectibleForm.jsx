fa1f999b19ac579bd0cab7a7dac47619e1d10d5c
Michelle Primiani <primianimichelle@gmail.com>
2023-12-07 11:59:59 -0600

    Post New Item fetch error

--- src/components/collectibles/CreateCollectibleForm.jsx
+++ src/components/collectibles/CreateCollectibleForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getAllCollectibles } from "../managers/CollectibleManager"
 import { Button } from "@radix-ui/themes"
+import { useNavigate } from "react-router-dom"
 
 
 
@@ -13,9 +14,22 @@ export const CreateCollectibleForm = () => {
         color: "",
         size: ""
     }
+    const initImageState1 = {
+        img_url: ""
+    }
+    const initImageState2 = {
+        img_url: ""
+    }
+    const initImageState3 = {
+        img_url: ""
+    }
+    const [image1, updateImage1] = useState(initImageState1)
+    const [image2, updateImage2] = useState(initImageState2)
+    const [image3, updateImage3] = useState(initImageState3)
     const [item, updateItem] = useState(initItemState)
     const [chosenCategories, updateChosen] = useState(new Set())
     const [categories, changeCategories] = useState([{ id: 1, name: "Art & Collectibles"}, {id: 2, name: "Home & Living"}])
+    const navigate = useNavigate()
 
     const fetchCategories  = async () => {
         const response = await fetch("http://localhost:8000/categories", {
@@ -29,14 +43,13 @@ export const CreateCollectibleForm = () => {
 
     const postCollectible = async (evt) => {
         evt.preventDefault()
-
-        await fetch(`http://localhost:8000/collectible`, {
+        await fetch(`http://localhost:8000/collectibles`, {
             method: "POST",
             headers: {
                 "Authorization": `Token ${localStorage.getItem("auth_token")}`,
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({...item, categories: Array.from(chosenCategories)})
+            body: JSON.stringify({...item, images: image1, image2, image3, categories: Array.from(chosenCategories)})
         })
         await getAllCollectibles()
         navigate("/")
@@ -51,6 +64,11 @@ export const CreateCollectibleForm = () => {
         copy.has(category.id) ? copy.delete(category.id) : copy.add(category.id)
         updateChosen(copy)
     }
+
+    const handleImageInput1 = (e) => updateImage1({ img_url: e.target.value });
+    const handleImageInput2 = (e) => updateImage2({ img_url: e.target.value });
+    const handleImageInput3 = (e) => updateImage3({ img_url: e.target.value });
+    
 
     const handleUserInput = (e) => updateItem({ ...item, [e.target.id]: e.target.value })
 
@@ -87,6 +105,37 @@ export const CreateCollectibleForm = () => {
                             <label htmlFor="size">Size:</label>
                             {formInput("size")}
                         </fieldset>
+                        <div className="images">
+                        Please Upload Up To Three Images
+                        <fieldset>
+                        <label htmlFor="image1">Image 1:</label>
+                        <input
+                            id="image1"
+                            type="text"
+                            className="form-control"
+                            onChange={(e) => handleImageInput1(e, 0)}
+                        />
+                        </fieldset>
+                        <fieldset>
+                        <label htmlFor="image2">Image 2:</label>
+                        <input
+                            id="image2"
+                            type="text"
+                            className="form-control"
+                            onChange={(e) => handleImageInput2(e, 1)}
+                        />
+                        </fieldset>
+                        <fieldset>
+                        <label htmlFor="image3">Image 3:</label>
+                        <input
+                            id="image3"
+                            type="text"
+                            className="form-control"
+                            onChange={(e) => handleImageInput3(e, 2)}
+                        />
+                        </fieldset>
+
+                        </div>
                         <fieldset>
                             <div className="form-group">
                                 <div>Categories:</div>
@@ -104,7 +153,7 @@ export const CreateCollectibleForm = () => {
                         type="submit"
                         onClick={postCollectible}
                         >
-                            Add Book
+                            Post Item
                         </Button>
                     </form>
                 </section>
