6e38140a7c4aae10e5abafa5a7976721cc383446
Michelle Primiani <primianimichelle@gmail.com>
2023-12-12 13:32:51 -0600

    categories moved to navbar

--- src/components/collectibles/CollectiblesPage.jsx
+++ src/components/collectibles/CollectiblesPage.jsx
@@ -1,46 +1,21 @@
 import { useEffect, useState } from "react";
 import { getAllCollectiblesAndUser } from "../managers/CollectibleManager";
-import { getAllCategories } from "../managers/CategoriesManager";
-import { CategoryFilter } from "./CategoryFilter";
 import { CollectiblesList } from "./CollectiblesList";
 
-
-
-export const CollectiblesPage = () => {
+export const CollectiblesPage = ({ categories, selectedCategory }) => {
   const [collectibles, setCollectibles] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('all');
-  const [filteredCollectibles, setFilteredItems] = useState([]);
-
+  const [filteredCollectibles, setFilteredCollectibles] = useState([]);
 
   useEffect(() => {
     getAllCollectiblesAndUser().then(setCollectibles);
-    getAllCategories().then(setCategories);
   }, []);
 
   useEffect(() => {
-    if (selectedCategory === 'all') {
-      setFilteredItems(collectibles);
-    } else {
-      // Parse selectedCategory as an integer
-      const selectedCategoryId = parseInt(selectedCategory);
-      // Filter collectibles where the categories array includes the selectedCategoryId
-      const filtered = collectibles.filter(collectible => 
-        collectible.categories.includes(selectedCategoryId)
-      );
-  
-      setFilteredItems(filtered);
-    }
+    const filtered = selectedCategory === 'all' 
+      ? collectibles 
+      : collectibles.filter(collectible => collectible.categories.includes(parseInt(selectedCategory)));
+    setFilteredCollectibles(filtered);
   }, [selectedCategory, collectibles]);
 
-  return (
-    <>
-      <CategoryFilter
-        categories={categories} 
-        setSelectedCategory={setSelectedCategory} 
-        selectedCategory={selectedCategory} 
-      />
-      <CollectiblesList collectibles={filteredCollectibles} />
-    </>
-  );
+  return <CollectiblesList collectibles={filteredCollectibles} />;
 };
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,10 +4,10 @@ import { useEffect, useState } from "react";
 import { getPixoUserById } from "../managers/PixoUserManager";
 import { getAllCategories } from "../managers/CategoriesManager";
 import { ProfileDropDown } from "./ProfileDropDown";
+import { CategoryFilter } from "../collectibles/CategoryFilter";
 
 export const NavBar = ( {userId, setToken, categories, selectedCategory, setSelectedCategory }) => {
   const [pixoUser, setPixoUser] = useState([])
-  const navigate = useNavigate();
 
 
   useEffect(() => {
@@ -25,6 +25,8 @@ export const NavBar = ( {userId, setToken, categories, selectedCategory, setSele
               <span className="ml-2 text-2xl font-bold">Pixo</span>
             </NavLink>
           </div>
+          <CategoryFilter className=""
+          categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
           <div className="m-5 flex-shrink-0 flex items-center">
             <ProfileDropDown userId={userId} pixoUser={pixoUser} setToken={setToken}/>
           </div>
