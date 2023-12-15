7a8d6ff2f2dad2d98bf27ebf56f2542a266b8a7f
Michelle Primiani <primianimichelle@gmail.com>
2023-12-15 12:52:46 -0600

    wip messages

--- src/components/managers/MessagesManager.jsx
+++ src/components/managers/MessagesManager.jsx
@@ -11,3 +11,16 @@ export const getMessagesByUser = async () => {
     })
     return await response.json()
 }
+
+
+export const createMessage = async (newMessage) => {
+    const response = await fetch('http://localhost:8000/messages', {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMessage),
+    });
+    return response;
+}
